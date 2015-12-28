46da2f7339a08c8ad7dc774711208ece7127ea55
InBlast <tristan.couble@gmail.com>
2015-12-28 22:48:28 +0100

    Update IB_Berserker.js

--- IB_Berserker.js
+++ IB_Berserker.js
@@ -4,12 +4,12 @@
  
 
 /*:
- * @plugindesc v1.00 Boost the physical damage of an actor based on a formula.
+ * @plugindesc v1.00 Boost the damages of an actor based on a formula.
  * @author InBlast  
  *       
  *
  * @param PhysicalFormula
- * @desc Change the physical damages by this formula :    
+ * @desc Change the damages by this formula :    
  * Default: 2 - this.subject().hpRate()
  * @default 2 - this.subject().hpRate()
  *
@@ -25,10 +25,15 @@
  * Use "this.subject()" instead of "a" to use parameters in the formulas.
  * example : this.subject().hp is the same as a.hp
  * --------------------------------------------------------------------------------
+ * Actor Notetag
+ * --------------------------------------------------------------------------------
+ * <Berserker>
+ * Make the actor a "Berserker"
+ *
+ * --------------------------------------------------------------------------------
  * Version History
  * --------------------------------------------------------------------------------
  * 1.0 - Release
-
 
 
  */
@@ -44,8 +49,13 @@
   Game_Action.prototype.makeDamageValue = function(target, critical) {
 
     var value = _Game_Action_Damage.call(this, target, critical);    // Normal damage calculation
-  	 if (this.subject().isActor() && this.subject().actor().meta.Berserker) {            // Is the user a berserker ?
-      return Math.round(value *= eval(formula));                                    // Up the damages with the remaining HP
+  	 if (this.subject().isActor() && this.subject().actor().meta.Berserker) {    // Is the user a berserker ?
+      if(eval(formula) === 0)  {
+        return value;
+      } else {
+        return Math.round(value *= eval(formula));  // Up the damages with the remaining HP
+      }        
+                                          
      } else {
       return value;                                                      // Return the normal damages
      };
